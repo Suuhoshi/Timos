866cff3e5723f5d1bbf479d78888524250bc37d5
Hanako Yamada <h-yamada@example.com>
2026-06-12 11:23:45 +0900

    success_reserch

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -5,7 +5,9 @@ app_name="shopapp"
 
 urlpatterns =[
     path('', views.main, name="main"),
-    path('login/', views.Login.as_view(), name="login"),
+    path('search/', views.search, name="search"),
+    path('detail/<int:item_id>/', views.detail, name="detail"),
+    # path('login/', views.Login.as_view(), name="login"),
     path('cart/', views.cart),
     # path('signup_success', views.SignupSuccess.as_view(), name="signup_success"),
     # path('', views.register, name="M02"),
--- shopapp/views.py
+++ shopapp/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import LoginForm
-from .models import User
+from .models import Category, Item
 
 
 def main(request):
-    return render(request, 'main.html')
+    categories = Category.objects.all().order_by("category_id")
+    context ={
+        "categories":categories
+    }
+    return render(request, 'main.html', context)
 
 
+def search(request):
+    category_id = request.GET.get("category_id")
+    keyword = request.GET.get("keyword")
+
+    items=Item.objects.all()
+    category_name =""
+
+    if category_id:
+        # print('------------------------------')
+        # print(category_id)
+        # print(keyword)
+        # print('------------------------------')
+        category =Category.objects.get(category_id=category_id)
+        category_name=category.name
+        if category.category_id !=0:
+            # print('------------------------------')
+            # print(category)
+            # print('------------------------------')
+            items = items.filter(category=category)
+
+    if keyword:
+        items=items.filter(name__icontains=keyword)
+
+    context={
+        "items":items,
+        "category_name":category_name,
+        "keyword":keyword
+    }
+    return render(request, "searchResult.html",context)
+
+
+
+
+def detail(request, item_id):
+    print("---------------")
+    print("item_id")
+    item=Item.objects.get(item_id=item_id)
+    context={
+        "item_detail":item,
+    }
+    return render(request, "itemDetail.html", context)
 
 # class Login(View):
 #     def get(self, request):
