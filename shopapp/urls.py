9db994ba8a0f8b22c6cac547f3f4004d58283f01
sunkyutech <sato@example.com>
2026-06-17 13:01:52 +0900

    [add] admin functions

--- shopapp/urls.py
+++ shopapp/urls.py
@@ -22,6 +22,14 @@ urlpatterns =[
     #任意機能-----------------
     path('cart/delete/<int:pk>', views.cart_delete, name="cart_delete"),
     path('cart/update/<int:pk>', views.cart_update, name="cart_update"),
+    #管理者機能-----------------
+    path('admin/login/', views.admin_login, name="admin_login"),
+    path('admin/', views.admin_main, name="admin_main"),
+    path('admin/logout/', views.admin_logout, name="admin_logout"),
+    path('admin/item/register/', views.admin_item_register, name="admin_item_register"),
+    path('admin/item/update/<int:item_id>/', views.admin_item_update, name="admin_item_update"),
+    path('admin/item/delete/<int:item_id>/', views.admin_item_delete, name="admin_item_delete"),
+    path('admin/purchase/cancel/<int:parchase_id>/', views.admin_purchase_cancel, name="admin_purchase_cancel"),
 ]
 
 
