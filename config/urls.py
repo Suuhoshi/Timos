80ca7dd1a0a83964c3c906093df5e7aa76061a78
sunkyutech <sato@example.com>
2026-06-18 13:03:08 +0900

    [add] item images for searchResult and itemDetail

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from shopapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('shopapp/',include("shopapp.urls")),
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
